6a7d9f745fe958ebc3bc82e08e4b881db26564d9
4601104008 <117006658+4601104008@users.noreply.github.com>
2022-11-29 20:00:41 +0700

    Add project files.

---
+++ QLBanDienThoai/Login.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace QLBanDienThoai
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDangnhap = new System.Windows.Forms.Button();
+            this.txtTaikhoan = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMatkhau = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.chkpass = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnDangnhap
+            // 
+            this.btnDangnhap.Location = new System.Drawing.Point(34, 133);
+            this.btnDangnhap.Name = "btnDangnhap";
+            this.btnDangnhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangnhap.TabIndex = 0;
+            this.btnDangnhap.Text = "Đăng nhập";
+            this.btnDangnhap.UseVisualStyleBackColor = true;
+            this.btnDangnhap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTaikhoan
+            // 
+            this.txtTaikhoan.Location = new System.Drawing.Point(121, 47);
+            this.txtTaikhoan.Name = "txtTaikhoan";
+            this.txtTaikhoan.Size = new System.Drawing.Size(134, 20);
+            this.txtTaikhoan.TabIndex = 1;
+            this.txtTaikhoan.TextChanged += new System.EventHandler(this.txtTaikhoan_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tài khoản ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // txtMatkhau
+            // 
+            this.txtMatkhau.Location = new System.Drawing.Point(121, 85);
+            this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.PasswordChar = '*';
+            this.txtMatkhau.Size = new System.Drawing.Size(134, 20);
+            this.txtMatkhau.TabIndex = 4;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(154, 133);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 5;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // chkpass
+            // 
+            this.chkpass.AutoSize = true;
+            this.chkpass.Location = new System.Drawing.Point(121, 112);
+            this.chkpass.Name = "chkpass";
+            this.chkpass.Size = new System.Drawing.Size(108, 17);
+            this.chkpass.TabIndex = 6;
+            this.chkpass.TabStop = true;
+            this.chkpass.Text = "Hiển thị mật khẩu";
+            this.chkpass.UseVisualStyleBackColor = true;
+            this.chkpass.CheckedChanged += new System.EventHandler(this.chkpass_CheckedChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 186);
+            this.Controls.Add(this.chkpass);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.txtMatkhau);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTaikhoan);
+            this.Controls.Add(this.btnDangnhap);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDangnhap;
+        private System.Windows.Forms.TextBox txtTaikhoan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMatkhau;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.RadioButton chkpass;
+    }
+}
---
+++ QLBanDienThoai/Login.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLBanDienThoai
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        if (Kiemtradangnhap(txtTaikhoan.Text,txtMatkhau.Text))
+            {
+                Main f=new Main();  
+                f.Show();
+                this.Hide();
+               
+            }
+        else
+            {
+                MessageBox.Show("Tài khoản mật khẩu không chính xác");
+
+            }
+        }
+        string tentaikhoan = "admin";
+        string matkhau="admin"; 
+        bool Kiemtradangnhap(string tentaikhoan,string matkhau)
+        {
+            if (tentaikhoan == this.tentaikhoan && matkhau == matkhau)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void txtTaikhoan_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkpass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkpass.Checked)
+            {
+                txtMatkhau.PasswordChar = (char)0;
+            }
+            else
+            {
+                txtMatkhau.PasswordChar = '*';
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dg = MessageBox.Show("Bạn có muốn thoát ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dg == DialogResult.Yes)
+                Application.Exit();
+        }
+    }
+}
